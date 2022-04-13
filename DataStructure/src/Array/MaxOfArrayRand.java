76a61133136279b1fd280cf6b40cf4bc04da0f04
gon <'gonsland99@gmail.com'>
2022-04-13 20:02:49 +0900

    난수 최대값 구하기

---
+++ DataStructure/src/Array/MaxOfArrayRand.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.*;
+
+public class MaxOfArrayRand {
+	static int maxOf(int[] a) {
+		int max = a[0];
+		for(int i=1; i<a.length; i++) {
+			if(a[i] > max)
+				max = a[i];
+		}
+		return max;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
+		
+		System.out.println("제일 큰 수 찾기(1~50)");
+		System.out.println("숫자 개수");
+		int count = sc.nextInt();
+		
+		int[] nums = new int[count];
+		
+		System.out.println("숫자 목록--------");
+		for(int i=0; i<count; i++) {
+			nums[i] = rand.nextInt(50)+1;
+			System.out.print(nums[i]+" ");
+		}
+		System.out.println("최대값: "+maxOf(nums));
+		
+	}
+}
