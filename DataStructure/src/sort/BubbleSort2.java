69575e85c60443b1bf5077f22491794b608a8661
gon <'gonsland99@gmail.com'>
2022-05-10 16:36:46 +0900

    버블정렬

---
+++ DataStructure/src/sort/BubbleSort2.java
@@ -0,0 +1,38 @@
+package sort;
+
+import java.util.Scanner;
+
+public class BubbleSort2 {
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	static void bubbleSort(int[] a, int n) {
+		for(int i=0; i<n-1; i++) {
+			int exchg = 0;
+			for(int j=n-1; j>i; j--)
+				if(a[j-1] > a[j]) {
+					swap(a, j-1, j);
+					exchg++;
+				}
+			if(exchg == 0) break;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("-----버블정렬-----");
+		System.out.print("총 개수: ");
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			System.out.print("a["+i+"]: ");
+			a[i] = sc.nextInt();
+		}
+		bubbleSort(a, n);
+		for(int i=0; i<n-1; i++)
+			System.out.print(a[i]+" ");
+		
+	}
+}
---
+++ DataStructure/src/sort/BubbleSort3.java
@@ -0,0 +1,40 @@
+package sort;
+
+import java.util.Scanner;
+
+public class BubbleSort3 {
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	static void bubbleSort(int[] a, int n) {
+		int k = 0;
+		while(k <  n-1) {
+			int last = n-1;
+			for(int j=n-1; j>k; j--)
+				if(a[j-1] > a[j]) {
+					swap(a, j-1, j);
+					last = j;
+				}
+			k = last;
+		}
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("-----버블정렬-----");
+		System.out.print("총 개수: ");
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			System.out.print("a["+i+"]: ");
+			a[i] = sc.nextInt();
+		}
+		bubbleSort(a, n);
+		for(int i=0; i<n-1; i++)
+			System.out.print(a[i]+" ");
+		
+	}
+}
