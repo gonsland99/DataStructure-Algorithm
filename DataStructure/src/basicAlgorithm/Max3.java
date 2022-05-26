db2f66a4393132f0d1d161c25ca778d248744513
gon <'gonsland99@gmail.com'>
2022-05-26 15:05:18 +0900

    세 정수중 최대값 구하기

--- DataStructure/src/basicAlgorithm/Max3.java
+++ DataStructure/src/basicAlgorithm/Max3.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class Max3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("최대값 구하기");
-		System.out.print("a: "); int a = sc.nextInt();
-		System.out.print("b: "); int b = sc.nextInt();
-		System.out.print("c: "); int c = sc.nextInt();
+		System.out.println("-----세정수 중 최대값 구하기-----");
+		System.out.print("1번째 정수: "); int a = sc.nextInt();
+		System.out.print("2번째 정수: "); int b = sc.nextInt();
+		System.out.print("3번째 정수: "); int c = sc.nextInt();
 		
 		int max = a;
-		if(b>max) max=b;
-		if(c>max) max=c;
+		if(b > max) max = b;
+		if(c > max) max = c;
 		
-		System.out.println("최대값은 "+max);
+		System.out.println("최댓값은 "+max);
 	}
 }
