26fb073434b94ff737314398c1d2e8c7de6528d6
gon <'gonsland99@gmail.com'>
2022-05-26 15:05:56 +0900

    세정수 중 중간값 구하기

---
+++ DataStructure/src/basicAlgorithm/Median.java
@@ -0,0 +1,22 @@
+package basicAlgorithm;
+
+public class Median {
+	static int mid(int a, int b, int c) {
+		if(a >= b)
+			if(b >= c)
+				return b;
+			else if(a <= c)
+				return a;
+			else
+				return c;
+		else if(a > c)
+			return a;
+		else if(b > c)
+			return c;
+		else
+			return b;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
