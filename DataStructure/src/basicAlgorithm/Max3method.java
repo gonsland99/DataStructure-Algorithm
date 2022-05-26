bb94dea3f39e86c2dfef0b75ea9c195e98411f98
gon <'gonsland99@gmail.com'>
2022-05-26 15:05:37 +0900

    메서드를 이용한 세정수 중 최대값 구하기

---
+++ DataStructure/src/basicAlgorithm/Max3method.java
@@ -0,0 +1,17 @@
+package basicAlgorithm;
+
+public class Max3method {
+	
+	static int max(int a, int b, int c) {
+		int max = a;
+		if(b > max) max = b;
+		if(c > max) max = c;
+		return max;
+	}
+	public static void main(String[] args) {
+		System.out.println(max(1, 2, 3));
+		System.out.println(max(1, 2, 5));
+		System.out.println(max(3, 2, 1));
+		System.out.println(max(1, 3, 2));
+	}
+}
